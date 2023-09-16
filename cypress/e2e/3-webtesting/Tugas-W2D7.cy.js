10ce227bde6dfc7e29a28accfa8e8b2ad58f2da9
hafizbagasp <hafizbagasp@gmail.com>
2023-09-17 01:26:17 +0700

    Commit tugas W2 D7

--- cypress/e2e/3-webtesting/Tugas-W2D7.cy.js
+++ cypress/e2e/3-webtesting/Tugas-W2D7.cy.js
@@ -2,8 +2,7 @@ describe('Tugas Pekan 2 Day 7', () => {
     beforeEach(() => {
         cy.visit('https://katalon-demo-cura.herokuapp.com/')
     })
-// test 1
-// test 2
+
   it('Berhasil Login', () => {
     cy.get('#btn-make-appointment').click()
     cy.get('#txt-username').type('John Doe')
@@ -12,7 +11,7 @@ describe('Tugas Pekan 2 Day 7', () => {
     cy.get('h2').should('contain.text', 'Make Appointment')
   })
 
-  it('Gagal Login - Wrong Credentials', () => {
+  it.skip('Gagal Login - Wrong Credentials', () => {
     cy.get('#btn-make-appointment').click()
     cy.get('#txt-username').type('John Doe')
     cy.get('#txt-password').type('PasswordSalah')
@@ -20,11 +19,36 @@ describe('Tugas Pekan 2 Day 7', () => {
     cy.get('.text-danger').should('contain.text', 'Login failed!')
   })
 
-  //Misalkan fitur baru sedang tahap dev
-  it.only('Gagal Login - Empty Username and Password', () => {
+  it.skip('Gagal Login - Empty Username and Password', () => {
     cy.get('#btn-make-appointment').click()
     cy.get('#btn-login').click()
     cy.get('.text-danger').should('contain.text', 'Login failed!')
+  })
+
+  //Web Testing dari Login - Book Appointment - Logout (PASSED)
+  it.only('Book Appointment', () => {
+    //Berhasil Login
+    cy.get('#btn-make-appointment').click()
+    cy.get('#txt-username').type('John Doe')
+    cy.get('#txt-password').type('ThisIsNotAPassword')
+    cy.get('#btn-login').click()
+    cy.get('h2').should('contain.text', 'Make Appointment')
+
+    //Input Book Appointment
+    //cy.get('#combo_facility').click()
+    cy.get('#chk_hospotal_readmission').check()
+    cy.get('#radio_program_none').check()
+    cy.get('#txt_visit_date').type('09/09/2023')
+    //cy.get('#txt_comment').type('Coba Komen')
+    cy.get('#btn-book-appointment').click()
+    cy.get('h2').should('contain.text', 'Appointment Confirmation')
+
+    //Logout
+    cy.get('.text-center > .btn').click()
+    cy.get('#menu-toggle').click()
+    cy.get(':nth-child(6) > a').click()
+    cy.get('h1').should('contain.text', 'CURA Healthcare Service')
+
   })
 
 })
